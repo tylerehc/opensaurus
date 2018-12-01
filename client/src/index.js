bfba916c85469191037327d8de2fbbcb96d073a7
Tyler Crimi <tyler@catalyzemarketing.com>
2018-12-01 13:05:18 +0100

    add router

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter, Route, Link } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = document.getElementById('root');
+
+ReactDOM.render(
+  <HashRouter history={History}>
+    <Route path="/" component={App}>
+
+    </Route>
+  </HashRouter>
+, app);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

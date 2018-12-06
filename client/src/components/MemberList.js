8b5041c3b0daa175b8d62dba2d02390d6db80ce7
Tyler Crimi <tyler@catalyzemarketing.com>
2018-12-06 12:57:52 +0100

    render token count in member list component

--- client/src/components/MemberList.js
+++ client/src/components/MemberList.js
@@ -19,11 +19,24 @@ class MemberList extends Component {
   render() {
     const { members } = this.props.member;
     const { tokens } = this.props.token;
+
+    // count for each member
+    const tokenStore = {}
+    tokens.forEach(token => {
+      if (token.tokenOwner in tokenStore)  {
+        tokenStore[token.tokenOwner] += 1;
+      } else {
+        tokenStore[token.tokenOwner] = 1;
+      }
+    })
+
+    //
     return(
       <Container style={{marginBottom: 50}}>
         <ListGroup>
           <TransitionGroup className="member-list">
-            {members.map(({ _id, name }) => (
+            {members.map(
+              ({ _id, name }) => (
                 <CSSTransition key={_id} timeout={500} classNames="fade">
                   <ListGroupItem>
                     <Button
@@ -35,9 +48,9 @@ class MemberList extends Component {
                       &times;
                     </Button>
 
-
-
                     <div className="inline"><div className="field-label">Member:</div> {name}</div>
+                    <div className="inline"><div className="field-label">Token count:</div> {tokenStore[name] || 0}</div>
+                    <div></div>
                   </ListGroupItem>
                 </CSSTransition>
             ))}

f25db7a111c29a6989e17697b1032364330e0bef
Tyler Crimi <tyler@catalyzemarketing.com>
2018-12-18 21:27:59 +0100

    divid about text between OPS and BRO

--- client/src/pages/Networks.js
+++ client/src/pages/Networks.js
@@ -36,17 +36,6 @@ class About extends Component {
               </CardBody>
             </Card>
           </Col>
-          <Col xs="6">
-            <Card>
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </Col>
         </Row>
         </Container>
       </div>

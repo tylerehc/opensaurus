080bb527dbd3205b502064ac50856759b4725133
Tyler Crimi <tyler@catalyzemarketing.com>
2018-12-18 15:16:33 +0100

    a commit to push to heroku

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 // Components
 import MemberList from '../components/MemberList';
-import MemberModal from '../components/MemberModal';
+// import MemberModal from '../components/MemberModal';
 
 // Styling
 import { Container } from 'reactstrap';
@@ -13,7 +13,7 @@ class Members extends Component {
     return (
       <div className="Tasks">
         <Container>
-          <MemberModal />
+
           <MemberList />
         </Container>
       </div>
